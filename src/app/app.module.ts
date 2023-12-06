a969edb1616ca0d8c8affc6c7b1ccbb26d442793
BigDaddyMM <muhammedmehmedovic2001@hotmail.com>
2023-12-06 11:05:18 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { TipsComponent } from './tips/tips.component';
+import { SupportComponent } from './support/support.component';
+import { TaketestComponent } from './taketest/taketest.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    TipsComponent,
+    SupportComponent,
+    TaketestComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/taketest/taketest.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-taketest',
+  templateUrl: './taketest.component.html',
+  styleUrls: ['./taketest.component.css']
+})
+export class TaketestComponent {
+
+}
