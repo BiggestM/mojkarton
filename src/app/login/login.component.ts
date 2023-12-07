6c33ce16fb318b283f5e98181f7a907f2abf9d93
BiggestM <muhamed.mehmedovic@stu.ssst.edu.ba>
2023-12-08 00:20:39 +0100

    added savjeti

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor(private router: Router) {}
 
+  login() {
+    // Placeholder for signup logic, replace with actual signup logic
+
+    // After successful signup, navigate to the desired page (e.g., dashboard)
+    this.router.navigate(['']);
+}
 }
